132bc2682d69cc1e5590919a06b58f760193de39
Kyle Kelley <rgbkrk@gmail.com>
2015-10-15 23:36:55 -0500

    Make sure workspace is clean.

--- release.sh
+++ release.sh
@@ -4,14 +4,20 @@ set -euo pipefail
 if [ ! -e "$( which github-release )" ]; then
   echo "You need github-release installed."
   echo "go get github.com/aktau/github-release"
-  exit 2
+  exit 1
 fi
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 
 if [ "$BRANCH" != "master" ]; then
   echo "Must release from master branch"
-  exit 1
+  exit 2
+fi
+
+git diff --exit-code > /dev/null
+if [ $? != 0 ]; then
+  echo "Workspace is not clean. Exiting"
+  exit 3
 fi
 
 declare -xr ORG="rackerlabs"
