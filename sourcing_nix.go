87fc973a56c7849ad2c7f53e44951f5a33edd02b
Kyle Kelley <rgbkrk@gmail.com>
2015-10-21 05:38:19 -0500

    Trim a trailing slash.

--- sourcing_nix.go
+++ sourcing_nix.go
@@ -9,7 +9,7 @@ import (
 
 func sourceHelpString(basepath string) string {
 	s := "#\n"
-	s += fmt.Sprintf("# Credentials written to \"%s\"/\n", basepath)
+	s += fmt.Sprintf("# Credentials written to \"%s\"\n", basepath)
 	s += "#\n"
 	s += fmt.Sprintf("source \"%v\"\n", path.Join(basepath, "docker.env"))
 	s += fmt.Sprintf("# Run the command above to get your Docker environment variables set\n")
