a98fae70642ab2deac4315b600db80e592c126fd
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-07-21 17:01:44 -0500

    List make-swarm clusters

--- adapters/make-swarm.go
+++ adapters/make-swarm.go
@@ -2,39 +2,79 @@ package adapters
 
 import (
 	"fmt"
+	"strconv"
 	"text/tabwriter"
+	"github.com/getcarina/libcarina"
+	"github.com/pkg/errors"
+	"time"
 )
 
-type Carina struct {
+type MakeSwarm struct {
 	Credentials UserCredentials
 	Output *tabwriter.Writer
 }
 
-func (carina *Carina) LoadCredentials(credentials UserCredentials) error {
+const httpTimeout = time.Second * 15
+
+func (carina *MakeSwarm) LoadCredentials(credentials UserCredentials) error {
 	carina.Credentials = credentials
 	return nil
 }
 
-func (carina *Carina) ListClusters() error {
-	fmt.Println("[DEBUG] Listing Carina clusters...")
-	/*
-	     clusterList, err := carina.ClusterClient.List()
-	   	if err != nil {
-	   		return err
-	   	}
+func (carina *MakeSwarm) authenticate() (*libcarina.ClusterClient, error) {
+	fmt.Println("[DEBUG][make-swarm] Authenticating...")
+	carinaClient, err := libcarina.NewClusterClient(carina.Credentials.Endpoint, carina.Credentials.UserName, carina.Credentials.Secret)
+	if err == nil {
+		carinaClient.Client.Timeout = httpTimeout
+	}
+	return carinaClient, err
+}
 
-	   	err = writeClusterHeader(carina.TabWriter)
-	   	if err != nil {
-	   		return err
-	   	}
+func (carina *MakeSwarm) ListClusters() error {
+	carinaClient, err := carina.authenticate()
+	if err != nil {
+		return errors.Wrap(err, "[make-swarm] Authentication failed")
+	}
 
-	   	for _, cluster := range clusterList {
-	   		err = writeCluster(carina.TabWriter, &cluster)
-	   		if err != nil {
-	   			return err
-	   		}
-	   	}
-	   	err = carina.TabWriter.Flush()
-	   	return err*/
-	return nil
+	fmt.Println("[DEBUG][make-swarm] Listing clusters...")
+	clusterList, err := carinaClient.List()
+	if err != nil {
+		return err
+	}
+
+	err = carina.writeClusterHeader()
+	if err != nil {
+		return err
+	}
+
+	for _, cluster := range clusterList {
+		err = carina.writeCluster(&cluster)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func (carina *MakeSwarm) writeCluster(cluster *libcarina.Cluster) error {
+	fields := []string{
+		cluster.ClusterName,
+		cluster.Flavor,
+		strconv.FormatInt(cluster.Nodes.Int64(), 10),
+		strconv.FormatBool(cluster.AutoScale),
+		cluster.Status,
+	}
+	return writeRow(carina.Output, fields)
+}
+
+func (carina *MakeSwarm) writeClusterHeader() error {
+	headerFields := []string{
+		"ClusterName",
+		"Flavor",
+		"Nodes",
+		"AutoScale",
+		"Status",
+	}
+	return writeRow(carina.Output, headerFields)
 }
