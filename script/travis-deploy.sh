db2ae2ea0ffe16cac2e7c7c8e54b12dadb4cae6e
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-02-03 08:52:22 -0600

    try out secure matrix env

---
+++ script/travis-deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# This is a silly workaround to allow us to use secure values for our deployments without losing the ability to do
+export RS_USERNAME=$1
+export RS_API_KEY=$2
+export RS_REGION_NAME=$3
