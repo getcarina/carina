e65ea122544d0eed18fb47d01feb6fb580be8ca5
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-02-03 13:23:58 -0600

    Test filtering cluster templates by name

---
+++ client/client_test.go
@@ -0,0 +1,31 @@
+package client_test
+
+import (
+	"testing"
+
+	"github.com/getcarina/carina/client"
+	"github.com/getcarina/carina/common"
+	"github.com/getcarina/carina/internal/clienttest"
+	"github.com/getcarina/carina/internal/commontest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilterTemplatesByName(t *testing.T) {
+
+	service := new(commontest.MockClusterService)
+	service.On("ListClusterTemplates").Return([]common.ClusterTemplate{
+		&commontest.StubClusterTemplate{Name: "Kubernetes 1.4.5 on LXC"},
+		&commontest.StubClusterTemplate{Name: "Swarm 1.11.2 on LXC"},
+	})
+	account := new(clienttest.MockAccount)
+	account.On("NewClusterService").Return(service, nil)
+
+	client := client.NewClient(false)
+	templates, err := client.ListClusterTemplates(account, "Kubernetes*")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	assert.Len(t, templates, 1)
+}
