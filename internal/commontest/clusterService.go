69493e7910285d6fdbfa4f48d957115befe26c08
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-02-03 13:23:45 -0600

    Define test mocks/stubs in an internal package
    * Share mocks across packages in an internal package that is not included
      in the final compiled binary.
    * Define tests in a *_test package, to avoid import cycles of doom.
    * Using the "testify" framework to help out with asserts and mocks.

---
+++ internal/commontest/clusterService.go
@@ -0,0 +1,34 @@
+package commontest
+
+import (
+	"github.com/getcarina/carina/common"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockClusterService struct {
+	common.ClusterService
+	mock.Mock
+}
+
+func (mock *MockClusterService) ListClusterTemplates() ([]common.ClusterTemplate, error) {
+	args := mock.Called()
+	return args.Get(0).([]common.ClusterTemplate), nil
+}
+
+type StubClusterTemplate struct {
+	Name     string
+	COE      string
+	HostType string
+}
+
+func (stub *StubClusterTemplate) GetName() string {
+	return stub.Name
+}
+
+func (stub *StubClusterTemplate) GetCOE() string {
+	return stub.COE
+}
+
+func (stub *StubClusterTemplate) GetHostType() string {
+	return stub.HostType
+}
